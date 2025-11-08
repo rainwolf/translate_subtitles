e71553743338749e8233b024cb3c6f6a7163df56
rainwolf <walied.othman@gmail.com>
2025-11-08 16:25:18 +0700

    don't translate if target language exists

--- main.py
+++ main.py
@@ -8,30 +8,32 @@ if __name__ == '__main__':
     path = 'some_unix_path'
     browser, width, height = None, None, None
     to_lang, from_lang = 'th', 'en'
-    files_list = glob.glob(f'{path}/*')
+    files_list = glob.glob(f'{path}/*.mkv')
     files_list = [f for f in files_list if len(list(filter(lambda x: f[:-7] in x, files_list))) == 1]
     for file in files_list:
         if not file.endswith('.mkv'):
             continue
         print(file)
         try:
-            id, convert = get_track_and_type(path=file)
+            id, convert, translation_needed = get_track_and_type(path=file, from_lang=from_lang, to_lang=to_lang)
         except Exception as e:
             print(f"Error getting track and type")
             os.rename(file, f"{file}-subtitles_error")
             continue
         extract_and_convert_ass_to_srt(path=file, track_id=id, convert=convert)
-        convert_to_docx(path=file)
-        if browser is not None:
-            change_browser_window_size(browser=browser, max_width=width, max_height=height)
-        def translate_file():
-            return translate(path=f'{file[:-3]}docx', from_lang=from_lang, to_lang=to_lang, browser=browser)
-        if browser is None:
-            browser, width, height = translate_file()
-        else:
-            browser, _, _ = translate_file()
-        # from time import sleep
-        # sleep(20)
-        convert_to_srt(path=file)
-        strip_and_add_subtitle(path=file, language='th')
-    browser.quit()
+        if translation_needed:
+            convert_to_docx(path=file)
+            if browser is not None:
+                change_browser_window_size(browser=browser, max_width=width, max_height=height)
+            def translate_file():
+                return translate(path=f'{file[:-3]}docx', from_lang=from_lang, to_lang=to_lang, browser=browser)
+            if browser is None:
+                browser, width, height = translate_file()
+            else:
+                browser, _, _ = translate_file()
+            # from time import sleep
+            # sleep(20)
+            convert_to_srt(path=file)
+        strip_and_add_subtitle(path=file, language=to_lang)
+    if browser is not None:
+        browser.quit()
